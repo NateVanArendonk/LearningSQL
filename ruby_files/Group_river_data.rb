03293a4c6a7757d05fb87c1a5e6dc8ecf0d68dca
Nathan VanArendonk <nathan.vanarendonk@gmail.com>
2022-03-08 16:18:07 -0800

    Generate OOP for Inner Join

--- ruby_files/Group_river_data.rb
+++ ruby_files/Group_river_data.rb
@@ -1,48 +1,155 @@
-# Models for each river 
-models = ['CanESM2', 'CCSM4', 'CNRM-CM5', 'CSIRO-Mk3-6-0', 'GFDL-ESM2M','HadGEM2-CC', 'IPSL-CM5A-MR', 'inmcm4', 'MIROC5']
-# RCP Scenarios
-rcp = ['RCP45', 'RCP85']
-# Hydrology models 
-hydro_model = ['PRMS_P1', 'VIC_P1', 'VIC_P2', 'VIC_P3']
-# Statistical downscaling method used 
-downscale = ['BCSD', 'MACA']
-# Rivers to analyze 
-rivers = ['AUB', 'CED', 'CS2', 'DUNDU', 'ELWPO', 'LAG', 'NFSTI', 'NOOFE', 'PUY', 'SKAMO', 'SKOPO', 'SNOMO']
+require 'pg'
 
+class River 
+	
+	attr_accessor :name, :rcp, :hydro, :downscale, :gcm
 
-# Step 1. Get intermodel max, mean, min and std for each river across the hydro models 
-river = rivers[0]
-downscale = downscale[0]
-rcp_scenario = rcp[1]
-model = models[0]
-hydro_model.each do |hydro|
-	table_name = "#{river}_#{model}_#{rcp_scenario}_BCSD_#{hydro}"
-	puts table_name
+	def initialize(name, rcp, hydro, downscale, gcm)
+		@name = name
+		@rcp = rcp
+		@hydro = hydro
+		@downscale = downscale
+		@gcm = gcm
+	end
+
+	def db_table_name
+		"#{name.downcase}_#{gcm.downcase}_#{rcp.downcase}_#{downscale.downcase}_#{hydro.downcase}"
+	end
 end
 
 
-# Test case I will merge the AUB data into a single table 
+class ConnectPSQL
+	
+	attr_accessor :connection_status
 
-            # table_name = "#aub_#{model}_#{rcp_scenario}_BCSD_#{hydro_scenario}"
-            # puts table_name
+	def initialize(args)
+		@host = args.fetch(:host)
+		@port = args.fetch(:port)
+		@dbname = args.fetch(:dbname)
+		@user = args.fetch(:user)
+		@password = args.fetch(:password)
+		@connection_status = args.fetch(:connection_status, 'closed')
+	end
+
+	def connect
+		# Connect to PSQL database
+		@connection_status = PG.connect(
+			:host => @host,
+			:port => @port, 
+			:dbname => @dbname, 
+			:user => @user,
+			:password => @password
+			)
+	end
+
+	def execute_query(query)
+		@connection_status.exec(query)
+	end
+
+	def close
+		@connection_status.close
+	end
+end
+
+
+class WritePSQLMessage
+	attr_accessor :message, :conection
+
+	def initialize(args)
+		@connection = args.fetch(:connection)
+		@message = args.fetch(:message,'')
+		@river = args.fetch(:river)
+	end
+
+	# Example
+	def write_simple_print
+		@message = "SELECT * FROM #{@river.db_table_name} LIMIT 10;"
+	end
+
+	# Write the join to average all the GCM runs for a single rcp/hydro/downscale scenario 
+	def generate_hash_for_gcm_join(gcm_list)
+		hash = {}
+		alphabet = ('a'..'z').to_a 
+		numbers = ('1'..'9').to_a
+		arr = []
+		
+		# Make unique array of letters and numbers 
+		alphabet.each do |letter|
+			numbers.each do |num|
+				arr << letter + num
+			end
+		end
+		arr = arr.shuffle
+
+		gcm_list.each do |gcm|
+			key = arr.shift
+			hash[key] = gcm
+		end
+		hash
+	end
+
+	def write_gcm_join(gcm_list)
+		hash = generate_hash_for_gcm_join(gcm_list)
+		# Make alias message for each gcm
+		alias = []
+		hash.each do |k, v|
+			alias << "#{k}.date#{v}"
 
 
 
 
 
-# # Connect to PSQL database
-# conn = PG.connect(
-#     :host => "localhost",
-#     :port => 5432, 
-#     :dbname => "discharge", 
-#     :user => "postgres", 
-#     :password => "password"
-#     )
+		@message = "SELECT * FROM #{@river.db_table_name}_#{hash.keys.join} LIMIT 10;"
+	end
+
+	# # Example 
+	# SELECT 
+	# l.date,l.streamflow,r.date,r.streamflow
+	# FROM t1 l 
+	# INNER JOIN t2 r
+	# on l.date = r.date
+
+end
 
 
 
 
 
 
-# # Close connection
-# conn.close
+
+
+
+
+
+# Example River object
+river = River.new('AUB', 'RCP85', 'VIC_P1', 'BCSD', 'CanESM2')
+
+# Connect to PSQL database
+db_connection = ConnectPSQL.new(
+	:host => 'localhost', 
+	:port => 5432,
+	:dbname => 'discharge',
+	:user => 'postgres',
+	:password => 'password'
+)
+db_connection.connect
+
+# Generate SQL query
+query = WritePSQLMessage.new(
+	:connection => db_connection,
+	:river => river
+)
+query.write_simple_print
+gcm_hash = query.generate_hash_for_gcm_join(['CanESM2', 'CCSM4', 'CNRM-CM5', 'CSIRO-Mk3-6-0', 'GFDL-ESM2M','HadGEM2-CC', 'IPSL-CM5A-MR', 'inmcm4', 'MIROC5'])
+# Execute SQL query
+# results = db_connection.execute_query(query.message)
+
+# Close connection
+db_connection.close
+
+# puts results.to_a
+
+test = ["Hello This is", "a test", "of the", "write_gcm_join method"]
+puts test
+puts test.join(",")
+
